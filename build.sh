965ee1319c8732c1c36830b1b3aa1318aea00f4e
artdeell <ultramaksim@gmail.com>
2026-01-08 13:43:06 +0300

    Feat[build]: add patches

--- build.sh
+++ build.sh
@@ -3,8 +3,13 @@ git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools
 export PATH=$PWD/depot_tools:$PATH
 mkdir angle
 cd angle
+
 fetch --no-history angle
+
 echo "target_os = ['android']" >> .gclient
 gclient sync --no-history
+
+find ../patches/ -name "*.diff" -print0 | xargs -0 -I {} sh -c 'echo "Applying {}" && git apply  --reject --whitespace=fix {} || (echo "Failed to apply patches" && exit 1)'
+
 mkdir out
 mkdir ../build_out
